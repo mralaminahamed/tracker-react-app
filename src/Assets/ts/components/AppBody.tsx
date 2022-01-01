c3ca5a8fa60dbce906d48220dd5fd96ef80e849d
Al-Amin Ahamed <alamin@mishusoft.com>
2022-01-01 10:11:21 +0600

    Update AppBody.tsx

--- src/Assets/ts/components/AppBody.tsx
+++ src/Assets/ts/components/AppBody.tsx
@@ -1,5 +1,5 @@
 /* external deps */
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 /* internal deps */
 import Home from "../pages/Home";
