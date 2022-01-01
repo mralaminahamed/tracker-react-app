fc2b0de7a9e0903b334d82cc3e0166ce0808a0e8
Al-Amin Ahamed <alamin@mishusoft.com>
2022-01-01 12:37:16 +0600

    added new api

---
+++ src/Assets/ts/includes/request.ts
@@ -0,0 +1,75 @@
+export function sendRequest(options: { method: string; url: string; async: boolean; header?: any[]; data?: any }, callback?: (arg0: string) => void, fallback?: (arg0: Error) => void) {
+    let dataType = "unrecognized";
+
+    if (typeof options === "object") {
+        if (options.method !== "" && options.url !== "") {
+            if (typeof XMLHttpRequest !== "undefined") {
+                let request = new XMLHttpRequest();
+                request.open(options.method, options.url, options.async);
+                //set header for xhr request
+                if (options.header !== undefined && typeof options.header == "object") {
+                    options.header.forEach((item) => {
+                        Object.keys(item).forEach((name) => {
+                            let value = item[name];
+                            request.setRequestHeader(name, value);
+                            if (value.indexOf("form") !== -1) {
+                                dataType = "formData";
+                            }
+                            if (value.indexOf("json") !== -1) {
+                                dataType = "jsonData";
+                            }
+                        });
+                    });
+                }
+
+                //send data with xhr request
+                if (dataType !== "unrecognized") {
+                    if (options.data !== undefined && typeof options.data == "object") {
+                        //send json data
+                        if (dataType === "jsonData") {
+                            request.send(JSON.stringify(options.data));
+                        }
+                        //send form data
+                        if (dataType === "formData") {
+                            let formData = new FormData();
+                            Object.keys(options.data).forEach(function (key) {
+                                formData.append(key, options.data[key]);
+                            });
+                            request.send(formData);
+                        }
+                    }
+                } else {
+                    request.send();
+                }
+
+                //catch sate of xhr
+                request.onreadystatechange = function () {
+                    if (this.readyState === 4) {
+                        if (this.status === 0) {
+                            if (fallback) {
+                                fallback(new Error(`Request send failed ${options.url}`));
+                            }
+                        }
+                        if (this.status === 200) {
+                            if (callback) {
+                                callback(this.responseText);
+                            }
+                        }
+                    }
+                };
+            } else {
+                if (fallback) {
+                    fallback(new Error("Runtime Environment could not support XMLHttpRequest"));
+                }
+            }
+        } else {
+            if (fallback) {
+                fallback(new Error("Request Method and URL can not be empty"));
+            }
+        }
+    } else {
+        if (fallback) {
+            fallback(new Error("Invalid options"));
+        }
+    }
+}
--- src/Assets/ts/pages/Diviflash.tsx
+++ src/Assets/ts/pages/Diviflash.tsx
@@ -1,13 +1,38 @@
-import {update_title} from "../includes/utilities";
+// external deps
+import { useEffect, useState } from "react";
+
+// internal deps
+import { sendRequest } from "../includes/request";
+import { update_title } from "../includes/utilities";
 
 export default function Diviflash() {
+    const [ipInfo, setIpinfo] = useState({});
+    const [browserInfo, setBrowserInfo] = useState({});
 
-    update_title('DiviFlash')
+    useEffect(() => {
+        update_title("DiviFlash");
+        sendRequest({ method: "get", url: "https://www.mishusoft.com/api/tools/ipinfo", async: true }, setIpinfo);
+        sendRequest({ method: "get", url: "https://www.mishusoft.com/api/tools/browser", async: true }, setBrowserInfo);
+    }, []);
 
     return (
         <div className="container-special">
             <h1>Welcome to Issue Tracker for EchoaSoft Projects</h1>
             <p>Realtime application for tracking issues</p>
+            <button
+                onClick={() => {
+                    alert(browserInfo);
+                }}
+            >
+                click for browser info
+            </button>
+            <button
+                onClick={() => {
+                    alert(ipInfo);
+                }}
+            >
+                click for ip info
+            </button>
         </div>
     );
 }
